d0d7bf63ece64e991b423f714c0735b4483cf1eb
40414245 <chair01@qub.ac.uk>
2026-07-23 16:59:18 +0100

    Started webpage component, added package.json start shortcut, added self signed https certificate

---
+++ components/webpage.js
@@ -0,0 +1,18 @@
+﻿// import { CSS3DRenderer } from 'three/addons/renderers/CSS3DRenderer.js';
+
+
+AFRAME.registerComponent('webpage', {
+    schema: {
+        url: {type: 'string'},
+        position: {type: 'string'},
+    },
+
+    init: function () {
+        console.log('Adding webpage');
+        const { data } = this;
+        const { element } = data;
+
+        // this.cssRenderer = new CSS3DRenderer()
+
+    }
+});
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+﻿import basicSsl from '@vitejs/plugin-basic-ssl'
+
+export default {
+    plugins: [
+        basicSsl({
+            /** name of certification */
+            name: 'vrtest',
+            /** optional, days before certificate expires */
+            ttlDays: 90,
+            /** custom certification directory */
+            certDir: './',
+        }),
+    ],
+}
