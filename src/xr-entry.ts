da1573c7b42f0e23bde4958c305420aa3fe379ad
40414245 <chair01@qub.ac.uk>
2026-08-17 15:38:54 +0100

    Replaced XR button

---
+++ src/xr-entry.ts
@@ -0,0 +1,15 @@
+﻿function enterVR () {
+    const scene = document.querySelector('a-scene');
+    if (scene) {
+        if (scene.hasLoaded) {
+            scene.enterVR();
+        } else {
+            scene.addEventListener('loaded', scene.enterVR);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#enter-vr-btn')
+        .addEventListener('click', enterVR);
+});
