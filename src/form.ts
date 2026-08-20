d610e4ad003f1aa3740c03f07f5d843102cf299b
40414245 <chair01@qub.ac.uk>
2026-08-20 16:18:13 +0100

    Improved UX of useSearch (automatically changes prefix)

---
+++ src/form.ts
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const targetUrl = <HTMLInputElement> document.getElementById('targetUrl');
+    const targetUrlLabel = document.querySelector('[for=targetUrl]');
+    const useSearch = <HTMLInputElement> document.getElementById('useSearch');
+    
+    useSearch.addEventListener('change', () => {
+        const currentUrl = targetUrl.value;
+        if (useSearch.checked) {
+            targetUrlLabel.textContent = 'Search';
+            removeProtocolPrefixes(targetUrl);
+        }
+        else {
+            targetUrlLabel.textContent = 'URL';
+            if (currentUrl.length === 0) {
+                targetUrl.value = 'https://';
+            }
+        }
+    });
+});
+
+const prefixes = ['https://', 'http://'];
+function removeProtocolPrefixes(input: HTMLInputElement) {
+    const currentValue = input.value;
+    for (const prefix of prefixes) {
+        if (currentValue.startsWith(prefix)) {
+            input.value = currentValue.replace(prefix, '');
+        }
+    }
+}
