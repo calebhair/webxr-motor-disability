3bad0df3f53991032bf02aa6593dcd9ed53bb746
40414245 <chair01@qub.ac.uk>
2026-08-20 16:50:42 +0100

    URL validation

--- src/form.ts
+++ src/form.ts
@@ -1,4 +1,6 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
+﻿const INVALID_URL_CLASS = 'invalid-url';
+
+document.addEventListener('DOMContentLoaded', () => {
     const targetUrl = <HTMLInputElement> document.getElementById('targetUrl');
     const targetUrlLabel = document.querySelector('[for=targetUrl]');
     const useSearch = <HTMLInputElement> document.getElementById('useSearch');
@@ -6,6 +8,7 @@
     useSearch.addEventListener('change', () => {
         const currentUrl = targetUrl.value;
         if (useSearch.checked) {
+            targetUrl.classList.remove(INVALID_URL_CLASS);
             targetUrlLabel.textContent = 'Search';
             removeProtocolPrefixes(targetUrl);
         }
@@ -15,6 +18,14 @@
                 targetUrl.value = 'https://';
             }
         }
+    });
+    
+    targetUrl.addEventListener('input', async () => {
+        if (useSearch.checked) return;
+        
+        const success = checkURL(targetUrl.value);
+        if (!success) targetUrl.classList.add(INVALID_URL_CLASS);
+        else targetUrl.classList.remove(INVALID_URL_CLASS);
     });
 });
 
@@ -26,4 +37,9 @@ function removeProtocolPrefixes(input: HTMLInputElement) {
             input.value = currentValue.replace(prefix, '');
         }
     }
+}
+
+const urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+function checkURL(url: string) {
+    return url.match(new RegExp(urlRegex));
 }
