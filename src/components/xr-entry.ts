c31aba49708fa02c73b9c8c39d87d8e3558f3441
40414245 <chair01@qub.ac.uk>
2026-08-20 14:50:36 +0100

    Forgot to commit; part of passthrough

--- src/components/xr-entry.ts
+++ src/components/xr-entry.ts
@@ -2,6 +2,10 @@
     const scene = document.querySelector('a-scene');
     if (!scene) return;
 
+    // Force VR if AR isn't supported
+    const arSupported = await navigator.xr.isSessionSupported('immersive-ar');
+    if (!arSupported) mode = 'vr';
+    
     const xrFunction = `enter${mode.toUpperCase()}`;
 
     if (scene.hasLoaded) scene[xrFunction]();
@@ -9,5 +13,5 @@
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#enter-vr-btn').addEventListener('click', () => enterXR('vr'));
+    document.querySelector('#enter-vr-btn').addEventListener('click', () => enterXR('ar'));
 });
